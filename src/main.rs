459f3c80b0b60d4c752956362fb4db860f5a8da2
meshde <meshde.md@gmail.com>
2024-04-07 17:02:13 +0400

    add basic code that reads GET calls to make from a config

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest;
+use serde_json::Value;
+use std::env;
+use std::fs::File;
+use std::io::BufReader;
+use std::io::Write;
+use std::io::stdout;
+use tokio;
+use colored_json;
+
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error> {
+    let file = File::open(".hitconfig.json").expect("config file missing");
+    let reader = BufReader::new(file);
+
+    let config: Value = serde_json::from_reader(reader).expect("Error while reading JSON");
+
+    let commands: Vec<&str> = config
+        .as_object()
+        .unwrap()
+        .keys()
+        .map(|key| key.as_str())
+        .collect();
+
+    let args: Vec<String> = env::args().collect();
+
+    if commands.contains(&args[1].as_str()) {
+        let command = args[1].as_str();
+        let api_call = config.get(command).unwrap();
+
+        if api_call.get("method").unwrap().as_str().unwrap() == "GET" {
+            let response: Value = serde_json::from_str(reqwest::get(api_call.get("url").unwrap().as_str().unwrap()).await?.text().await?.as_str()).unwrap();
+            let mut out = stdout();
+            colored_json::write_colored_json(&response, &mut out).unwrap();
+            writeln!(out, "").unwrap();
+            out.flush().unwrap();
+        } else {
+            println!("Not recognised method");
+        }
+    } else {
+        println!("command not recongnized");
+    }
+
+    Ok(())
 }
