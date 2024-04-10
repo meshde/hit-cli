6d7672687c1cad3ed74f80d055ff1f820fa128f3
meshde <meshde.md@gmail.com>
2024-04-10 08:53:12 +0400

    feat: add support for passing route params as cli options

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
+use colored_json;
+use convert_case::{Case, Casing};
+use getopts;
+use regex::Regex;
 use reqwest;
 use serde_json::Value;
+use std::collections::{HashMap, HashSet};
 use std::env;
 use std::fs::File;
+use std::io::stdout;
 use std::io::BufReader;
 use std::io::Write;
-use std::io::stdout;
+use std::process;
 use tokio;
-use colored_json;
 
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
@@ -14,6 +19,7 @@ async fn main() -> Result<(), reqwest::Error> {
     let reader = BufReader::new(file);
 
     let config: Value = serde_json::from_reader(reader).expect("Error while reading JSON");
+    let route_param_regex = Regex::new(r"\/:(\w+)").unwrap();
 
     let commands: Vec<&str> = config
         .as_object()
@@ -28,12 +34,68 @@ async fn main() -> Result<(), reqwest::Error> {
         let command = args[1].as_str();
         let api_call = config.get(command).unwrap();
 
+        let url = api_call
+            .get("url")
+            .expect("command missing url")
+            .as_str()
+            .expect("url is not string");
+
+        let route_params: Vec<&str> = route_param_regex
+            .captures_iter(url)
+            .filter_map(|caps| caps.get(1))
+            .map(|word| word.as_str())
+            .collect::<HashSet<&str>>()
+            .into_iter()
+            .collect();
+
+        let mut opts = getopts::Options::new();
+        for route_param in &route_params {
+            opts.optopt(
+                "",
+                &route_param.to_case(Case::Kebab),
+                &("the value for ".to_string() + route_param),
+                "",
+            );
+        }
+
+        let matches = match opts.parse(&args[2..]) {
+            Ok(m) => m,
+            Err(e) => {
+                eprintln!("{}", e.to_string());
+                process::exit(1);
+            }
+        };
+
+        let mut param_values: HashMap<&str, String> = HashMap::new();
+
+        for route_param in &route_params {
+            let kebab_cased_param = &route_param.to_case(Case::Kebab);
+            let param_value = match matches.opt_str(kebab_cased_param) {
+                Some(p) => p,
+                None => {
+                    eprintln!("Missing required option: {}", kebab_cased_param);
+                    process::exit(1);
+                }
+            };
+
+            param_values.insert(route_param, param_value);
+        }
+
         if api_call.get("method").unwrap().as_str().unwrap() == "GET" {
-            let response: Value = serde_json::from_str(reqwest::get(api_call.get("url").unwrap().as_str().unwrap()).await?.text().await?.as_str()).unwrap();
+            let response: Value = serde_json::from_str(
+                reqwest::get(route_params.iter().fold(url.to_string(), |acc, &x| {
+                    acc.replace(&(":".to_string() + x), &param_values.get(x).unwrap())
+                }))
+                .await?
+                .text()
+                .await?
+                .as_str(),
+            )
+            .unwrap();
             let mut out = stdout();
             colored_json::write_colored_json(&response, &mut out).unwrap();
-            writeln!(out, "").unwrap();
             out.flush().unwrap();
+            writeln!(out, "").unwrap();
         } else {
             println!("Not recognised method");
         }
